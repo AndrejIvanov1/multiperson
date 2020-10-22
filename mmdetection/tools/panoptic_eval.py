dc2df9518e6e626e87cb4287f4a03f9267e823fb
Andrej Ivanov <ivandrej@gmail.com>
2020-10-22 15:11:29 +0200

    Remove unnecessary dependency on H3.6M in evaluation script

--- mmdetection/tools/panoptic_eval.py
+++ mmdetection/tools/panoptic_eval.py
@@ -202,16 +202,17 @@ def main():
 
     model = build_detector(
         cfg.model, train_cfg=cfg.train_cfg, test_cfg=cfg.test_cfg)
-    train_dataset = get_dataset(cfg.datasets[0].train)
+    #train_dataset = get_dataset(cfg.datasets[0].train)
+    CLASSES = ("Human",)
     if cfg.checkpoint_config is not None:
         # save mmdet version, config file content and class names in
         # checkpoints as meta data
         cfg.checkpoint_config.meta = dict(
             mmdet_version=__version__,
             config=cfg.text,
-            CLASSES=train_dataset.CLASSES)
+            CLASSES=CLASSES)
     # add an attribute for visualization convenience
-    model.CLASSES = train_dataset.CLASSES
+    model.CLASSES = CLASSES
 
     model = MMDataParallel(model, device_ids=range(cfg.gpus)).cuda()
 
